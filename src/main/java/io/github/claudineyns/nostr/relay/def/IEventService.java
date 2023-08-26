5b2ef77109163975dd0e9ad25f66dcec5dee736a
Claudiney Nascimento <contato@claudiney.info>
2023-08-26 19:36:09 -0300

    Add validation service

--- src/main/java/io/github/claudineyns/nostr/relay/def/IEventService.java
+++ src/main/java/io/github/claudineyns/nostr/relay/def/IEventService.java
@@ -8,8 +8,8 @@ import io.github.claudineyns.nostr.relay.service.EventCacheDataService;
 import io.github.claudineyns.nostr.relay.specs.EventState;
 
 public interface IEventService {
-    static final String DB_ERROR     = "error: Could not connect to database";
-    static final String REG_REQUIRED = "blocked: pubkey must registered";
+    static final String DB_ERROR     = "error: Could not connect to database.";
+    static final String REG_REQUIRED = "blocked: pubkey must be registered.";
 
     // public static final IEventService INSTANCE = new EventDiskDataService();
     public static final IEventService INSTANCE = new EventCacheDataService();
