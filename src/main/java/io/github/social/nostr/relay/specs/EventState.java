87f27b23acb1c283f4cc2b004a6ca7cdce42627e
Claudiney Nascimento <contato@claudiney.info>
2023-08-27 05:44:35 -0300

    Add NIP-18

--- src/main/java/io/github/social/nostr/relay/specs/EventState.java
+++ src/main/java/io/github/social/nostr/relay/specs/EventState.java
@@ -7,7 +7,13 @@ public enum EventState {
     REGULAR, REPLACEABLE, EPHEMERAL, PARAMETERIZED_REPLACEABLE;
 
     public static EventState byKind(final int n) {
-        if ((1000 <= n && n < 10000) || Arrays.asList(EventKind.TEXT_NOTE, EventKind.DELETION, EventKind.REACTION).contains(n)) {
+        if ((1000 <= n && n < 10000) || Arrays.asList(
+                EventKind.TEXT_NOTE,
+                EventKind.DELETION,
+                EventKind.REACTION,
+                EventKind.REPOST,
+                EventKind.GENERIC_REPOST
+            ).contains(n)) {
             return REGULAR;
         }
 
